dc6cbc0fb5d898a9c04577139dadb8af5a85ffcf
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-03-11 23:51:03 -0300

    comparando referencias de objetos

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaTeti = new ContaCorrente();
+            contaDaTeti.titular = "Teti";
+            contaDaTeti.agencia = 863;
+            contaDaTeti.numero = 863452;
+
+            ContaCorrente contaDoDunga = new ContaCorrente();
+            contaDaTeti.titular = "Teti";
+            contaDaTeti.agencia = 863;
+            contaDaTeti.numero = 863452;
+
+            Console.WriteLine(contaDaTeti == contaDoDunga);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
